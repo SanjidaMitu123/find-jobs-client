f7b1952a8bd7929b34fb94a932926ca4ee37f324
SanjidaMitu123 <sanjidamitu456@gmail.com>
2023-11-05 17:46:48 +0600

    404 page  added

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -4,11 +4,13 @@ import {
   } from "react-router-dom";
 import Mainpage from "../pages/Mainpage";
 import Home from "../pages/Home";
+import ErrorPage from "../Errorpage";
 
 const router =   createBrowserRouter([
     {
       path: "/",
       element: <Mainpage></Mainpage>,
+      errorElement : <ErrorPage></ErrorPage>,
       children :[
         {
             path: "/",
